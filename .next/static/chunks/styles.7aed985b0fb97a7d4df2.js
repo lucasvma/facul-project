c286431b31013d5e77fde0edf3ed946e9da40588
Lucas Ventura <lucas.alves@mobiauto.com.br>
2021-02-02 00:25:34 -0300

    Change .gitignore

--- .next/static/chunks/styles.7aed985b0fb97a7d4df2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{hFjF:function(n,w,o){}}]);
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e7ebc888a4dfc7c6e7d";
+/******/ 	var hotCurrentHash = "78cb193eb1945d4d5a4b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
