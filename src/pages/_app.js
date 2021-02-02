328cf7d682324a20c7b881cded377df2488969cd
Lucas Ventura <lucas.alves@mobiauto.com.br>
2021-02-01 23:54:24 -0300

    Teste

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
+          <title>NextJS Material Kit by Lucas Ventura</title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
