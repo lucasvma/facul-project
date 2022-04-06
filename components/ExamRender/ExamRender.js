2d43aa59b0c21c7988cb8118edaa3a36a13c4199
Lucas Ventura <venturaml21@gmail.com>
2022-04-05 22:08:10 -0300

    - Adding some exam rules.

--- components/ExamRender/ExamRender.js
+++ components/ExamRender/ExamRender.js
@@ -31,53 +31,84 @@ async function readQuestion(exam) {
             if (line.substring(1, 2) === 'x') {
                 question.alternatives.push({
                     text: line.substring(3),
-                    isRight: true
+                    isRight: true,
+                    isChecked: false
                 })
                 return;
             }
             question.alternatives.push({
                 text: line.substring(2),
-                isRight: false
+                isRight: false,
+                isChecked: false
             })
         } else {
             question.title = line
         }
     })
-    console.log('questions', questions)
     return questions
+}
+
+async function checkResult(answers, template, minimumGrade) {
+    let reachedGrade = 0
+    answers.map((answer, index) => answer === template[index] ? reachedGrade += 1 : null)
+    return reachedGrade >= minimumGrade
 }
 
 export default function ExamRender(props) {
     const [exam, setExam] = useState(null)
-    const [minimumGrade, setMinimumGrade] = useState(props.minimumGrade)
+    const [template, setTemplate] = useState([])
+    const [answers, setAnswers] = useState([])
+    const minimumGrade = props.minimumGrade
     const [maxTime, setMaxTime] = useState(props.maxTime)
 
     useEffect(async () => {
-        if (props.exam !== '') {
+        if (props.exam !== null) {
             const actualExam = await readQuestion(props.exam)
             setExam(actualExam)
+            let returnedTemplate = []
+            exam?.map((question, questionIndex) => {
+                question.alternatives.map((alternative, alternativeIndex) => {
+                    if (alternative.isRight) {
+                        returnedTemplate[questionIndex] = alternativeIndex
+                    }
+                })
+            })
+            setTemplate(returnedTemplate)
         }
     }, [props.exam])
 
+    const setAnswer = (answer, answersInThePosition) => {
+        const newAnswers = answers
+        newAnswers[answersInThePosition] = answer
+        console.log('postion on', newAnswers)
+        setAnswers(newAnswers)
+        exam[answersInThePosition].alternatives[answer].isChecked = answers[answersInThePosition] === answer
+    }
+
+    const handleFinishExam = () => {
+        return checkResult(answers, template, minimumGrade)
+    }
+
     return exam === null
-        ? <><p>Não há alternativas</p></>
+        ? <><p>Carregando exame...</p></>
         : (
             <>
-                {exam.map((question) => (
+                {exam.map((question, questionIndex) => (
                     <>
                         <h2>
                             <Small>{question.title}</Small>
                         </h2>
-                        {question.alternatives.map((alternative) =>
+                        {question.alternatives.map((alternative, alternativeIndex) =>
                             (<>
                                 <FormControlLabel
                                     control={
                                         <Radio
-                                            checked={false}
-                                            // onChange={() => setSelectedEnabled("a")}
-                                            value="a"
+                                            defaultChecked={alternativeIndex === 0}
+                                            checked={exam[questionIndex].alternatives[alternativeIndex].isChecked}
+                                            onChange={() => setAnswer(alternativeIndex, questionIndex)}
+                                            value={alternativeIndex}
                                             name={`alternative-${question.title}`}
-                                            aria-label="A"
+                                            aria-label={`${question.title}`}
                                             icon={
                                                 <FiberManualRecord className={props.classes.radioUnchecked}/>
                                             }
